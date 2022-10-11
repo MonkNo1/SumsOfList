131fb110af0faa33d2c75841ca3d200d05120654
Monk <61575072+MonkNo1@users.noreply.github.com>
2022-10-11 23:17:16 +0530

    builtin upgrade

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ from itertools import combinations
 arr = [2,3,4,5,6,7,8]
 
 
-def sum_list(total, a):
+def sum_list(arr,total, a):
     h = []
-    c = [2,3,4,5,6,7,8]
-    print(c)
+    c = arr
     h = [combo for combo in combinations(c, a) if sum(combo) == total]
-    # return f'{total} contains of {a} elements : {h}'
-    print(f'{total} contains of {a} elements : {h}')
+    return h
 
-sum_list(12,3)
 
 def rem_dup(test_list):
     res = []
@@ -48,4 +45,15 @@ nlst.remove('na')
 print("The Nlst value is ")
 print(nlst)
 
-# dic = {}
+
+i = 1
+for x in nlst:
+    i = i + 1
+    # print(i)
+    # print(len(nlst))
+    if i > (len(nlst)+1):
+        break
+    # print(x)
+    kst = sum_list(arr,x,i)
+    print(kst)
+
