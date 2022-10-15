1949984247a6b033f89dee36b7446aff307cbcff
Monk <61575072+MonkNo1@users.noreply.github.com>
2022-10-15 20:36:25 +0530

    Convert to Single File

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import itertools 
 from itertools import combinations
-from arrSort import sortarr
 
+
+def sortarr(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
 arr = [3,5,2,7,4,2,3] 
 lst=[] 
